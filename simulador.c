fa1c847f16181bd57bb2bc69e300d6726f53ad43
Igor-dJS <igorjs@dcc.ufrj.br>
2023-11-26 16:06:52 -0300

    execucao parcialmente implementada

--- simulador.c
+++ simulador.c
@@ -2,29 +2,29 @@
 #include<stdlib.h>
 #include<time.h>
 
-/* VARI¡VEIS CONSTANTES */
-// TODOS OS TEMPOS EST√O EM UNIDADES DE TEMPO (U.T.)
+/* VARIÔøΩVEIS CONSTANTES */
+// TODOS OS TEMPOS ESTÔøΩO EM UNIDADES DE TEMPO (U.T.)
 
-#define QTDE_PROC 15		// N˙mero de processos
-#define TMAX 15				// Tempo m·ximo de serviÁo de um processo
+#define QTDE_PROC 15		// NÔøΩmero de processos
+#define TMAX 15				// Tempo mÔøΩximo de serviÔøΩo de um processo
 #define QUANTUM 3			// Fatia de tempo de cada processo na CPU 
 #define TEMPO_DISCO 2 		// Tempo de E/S para Disco 
-#define TEMPO_FITA 6 		// Tempo de E/S para Fita MagnÈtica
+#define TEMPO_FITA 6 		// Tempo de E/S para Fita MagnÔøΩtica
 #define TEMPO_IMPRESSORA 4 	// Tempo de E/S para Impressora
-#define PROB_IO 4			// Probabilidade com que um processo solicita IO na criaÁ„o (1/freq)
+#define PROB_IO 4			// Probabilidade com que um processo solicita IO na criaÔøΩÔøΩo (1/freq)
 
 typedef struct {
-	char tipo;				// Tipo de IO que vai ser solicitado (D, F, I) ou zero se n„o tem tipo
-	int inicio;				// Quando ocorrer· a solicitaÁ„o
-	int duracao;			// DuraÁ„o do IO
+	char tipo;				// Tipo de IO que vai ser solicitado (D, F, I) ou zero se nÔøΩo tem tipo
+	int inicio;				// Quando ocorrerÔøΩ a solicitaÔøΩÔøΩo
+	int duracao;			// DuraÔøΩÔøΩo do IO
 } IO;
 
  typedef struct PCB{
 	int pid;				// Identificador do processo
-	int tempo_serv;			// Tempo de serviÁo para o processo executar
+	int tempo_serv;			// Tempo de serviÔøΩo para o processo executar
 	int criacao;			// Tempo em que processo foi criado
 	IO* io;					// O momentos de IO do processo
-	struct PCB *prox, *ant;	// Aponta para o prÛximo elemento e para o elemento anterior
+	struct PCB *prox, *ant;	// Aponta para o prÔøΩximo elemento e para o elemento anterior
 } PCB;
 
 typedef struct {
@@ -44,9 +44,9 @@ int qtdeProcessos = 0;
 
 
 
-/* FunÁıes */
+/* FunÔøΩÔøΩes */
 
-//Alocar fila
+//Aloca fila
 FILA* aloca_fila(){
 	FILA* fila = (FILA *) malloc(sizeof(FILA));
 	fila->inicio = NULL;
@@ -56,7 +56,7 @@ FILA* aloca_fila(){
 	return fila;
 }
 
-// NOVO PROCESSO
+// Cria os processos
 FILA* gera_processos(){
     FILA* fila = aloca_fila();
     
@@ -64,7 +64,7 @@ FILA* gera_processos(){
 	int TIO[3] = {TEMPO_DISCO, TEMPO_FITA, TEMPO_IMPRESSORA};
     
     // Primeiro processo
-    fila->fim = (PCB *) malloc(sizeof(PCB));;
+    fila->fim = (PCB *) malloc(sizeof(PCB));
     
     fila->fim->prox = NULL;
 	fila->fim->ant = NULL;
@@ -72,10 +72,10 @@ FILA* gera_processos(){
     fila->inicio = fila->fim;
     fila->fim->pid = 1;
     
-   	fila->fim->tempo_serv = rand() % TMAX + 1; // Gera um valor aleatÛrio pro tempo de serviÁo
-   	fila->fim->criacao = rand() % 30;
+   	fila->fim->tempo_serv = rand() % TMAX + 1; // Gera um valor aleatorio para tempo de serv
+   	fila->fim->criacao = 0;
     
-    if(rand() % PROB_IO){ // Sorteia se o processo tem solicita√ß√£o de IO
+    if(rand() % PROB_IO){ // Sorteia se o processo tem IO
    		fila->fim->io = (IO *) malloc(sizeof(IO));
    		fila->fim->io->inicio = -1;
     	fila->fim->io->tipo = 0;
@@ -86,7 +86,7 @@ FILA* gera_processos(){
     	fila->fim->io = (IO *) malloc(sizeof(IO));
     	fila->fim->io->inicio = rand() % (fila->fim->tempo_serv + 1);
     	
-    	// Define o tipo de IO solicitado e o tempo de duraÁ„o do tipo de IO para o processo
+    	// Define o tipo de IO solicitado e o tempo de duracao do IO
     	fila->fim->io->tipo = tipo[t];
     	fila->fim->io->duracao = TIO[t];
     }
@@ -99,7 +99,7 @@ FILA* gera_processos(){
     	fila->inicio->prox = NULL;
     	fila->inicio->pid = i+1;
     	fila->inicio->tempo_serv = rand() % TMAX + 1;
-    	fila->inicio->criacao = rand() % 30;
+    	fila->inicio->criacao = fila->inicio->ant->criacao + (rand() % 5 + 1);
     	
     	if(rand() % PROB_IO){
     		fila->inicio->io = (IO *) malloc(sizeof(IO));
@@ -122,114 +122,7 @@ FILA* gera_processos(){
     
     return fila;
 }
-/*
-// FunÁ„o que troca o processo na base de filaA para o topo de filaB
-void troca_fila(PCB* processo, FILA* filaA, FILA* filaB){
-	if(filaB->topo != NULL && filaA->qtde > 1){
-		filaA->base = processo->prox;
-		filaA->base->ant = NULL;
-		
-		filaB->topo->prox = processo;
-		
-		processo->prox = NULL;
-		processo->ant = filaB->topo;
-		
-		filaB->topo = processo;
-	}
-	else if(filaB->topo != NULL && filaA->qtde == 1){
-		filaA->base = NULL;
-		filaA->topo = NULL;
-		
-		filaB->topo->prox = processo;
-		
-		processo->prox = NULL;
-		processo->ant = filaB->topo;
-		
-		filaB->topo = processo;
-	}
-	else if(filaB->topo == NULL && filaA->qtde > 1){
-		filaA->base = processo->prox;
-		filaA->base->ant = NULL;
-		
-		processo->prox = NULL;
-		processo->ant = NULL;
-		
-		filaB->base = processo;
-		filaB->topo = processo;
-	}
-	else{
-		filaA->base = NULL;
-		filaA->topo = NULL;
-		processo->prox = NULL;
-		processo->ant = NULL;
-		filaB->base = processo;
-		filaB->topo = processo;
-	}
-	filaA->qtde--;
-	filaB->numProcessos++;
-}
 
-// Fun√ß√£o para terminar processos
-void termina_processo(PCB* processo, FilaProcessos* fila){
-	if(fila->numProcessos == 1){
-		fila->base = NULL;
-		fila->topo = NULL;
-	}
-	else{
-		fila->base = fila->base->prox;
-		fila->base->ant = NULL;
-	}
-	printf("8: Processo %i conclu√≠do!\n", processo->pid);
-	free(processo->io);
-	free(processo);
-	fila->numProcessos--;
-	quantProc--;
-}
-
-// Fun√ß√£o que simula a execu√ß√£o do processo
-int executa_processo(PCB* processo){
-	int tempoPassado = 0;
-	if(!(rand() % PROBFILHO)) cria_filho(processo);
-	else puts("5: Nenhum processo filho foi criado");
-	if(processo->io->tipo){ // Verifica se o processo tem solicita√ß√£o de IO
-		if(processo->io->inicio - TRR <= 0){
-			tempoPassado = processo->io->inicio;
-			processo->tempo -= tempoPassado;
-			processo->io->inicio = -1;
-		}
-		else{
-			processo->io->inicio -= TRR;
-			processo->tempo -= TRR;
-			tempoPassado = TRR;
-		}
-	}
-	else{
-		if(processo->tempo - TRR <= 0){
-			tempoPassado = processo->tempo;
-			processo->tempo = -1;
-		}
-		else{
-			processo->tempo -= TRR;
-			tempoPassado = TRR;
-		}
-	}
-	return tempoPassado;
-}
-
-void imprime_status(){
-	if(filaIoDisco->numProcessos > 0) 
-		printf("1: Processo %i utilizando o Disco com %is restante(s)\n", filaIoDisco->base->pid, filaIoDisco->base->io->duracao);
-	if(filaIoFita->numProcessos > 0)
-		printf("2: Processo %i utilizando a Fita com %is restante(s)\n", filaIoFita->base->pid, filaIoFita->base->io->duracao);
-	if(filaIoImpressora->numProcessos > 0)
-		printf("3: Processo %i utilizando a Impressora com %is restante(s)\n", filaIoImpressora->base->pid, filaIoImpressora->base->io->duracao);
-	if(filaAltaPrioridade->numProcessos > 0) 
-		printf("4: Fila de Alta Prioridade: Processo %i a ser executado com %is restante(s)\n", filaAltaPrioridade->base->pid, filaAltaPrioridade->base->tempo);
-	else if(filaBaixaPrioridade->numProcessos > 0) 
-		printf("4: Fila de Baixa Prioridade: Processo %i a ser executado com %is restante(s)\n", filaBaixaPrioridade->base->pid, filaBaixaPrioridade->base->tempo);
-}
-
-*/
 
 void imprimirTabela(int tabela[][2], int LINHAS){
     for (int i = 0; i < LINHAS; i++) {
@@ -240,131 +133,227 @@ void imprimirTabela(int tabela[][2], int LINHAS){
     }
 }
 
-void retira_processo_fila(FILA* fila){
-	
+// Retira processo do inicio da fila
+PCB* retira_processo_fila(FILA* fila){
+	if (fila->inicio == NULL) {
+        return NULL; // Fila vazia
+    }
+
+    PCB* temp = fila->inicio;
+    fila->inicio = temp->ant;
+    
+    temp->ant = NULL;
+    temp->prox = NULL;
+
+    if (fila->inicio == NULL) {
+        fila->fim = NULL; // Fila agora esta vazia
+    }
+
+    fila->qtde--;
+
+    return temp;
 }
 
-void adiciona_processo_fila(FILA* fila){
-	
+// Vai add no fim da fila de alta e baixa prioridade
+void adiciona_processo_fila(FILA* fila, PCB* processo){
+	if (fila->inicio == NULL) {
+        fila->inicio = fila->fim = processo;
+        processo->ant = NULL;
+        processo->prox = NULL;
+    } else {
+    	processo->prox = fila->fim;
+    	
+        fila->fim->ant = processo;
+        fila->fim = processo;
+        
+        processo->ant = NULL;
+    }
+
+    fila->qtde++;
 }
+
+// Funcao para imprimir os elementos da fila
+void imprimirFila(FILA* fila) {
+    PCB* atual = fila->inicio;
+
+    printf("Elementos da fila: ");
+    while (atual != NULL) {
+        printf("%d ", atual->pid);
+        atual = atual->ant;
+    }
+    printf("\n");
+}
+
+void imprimirTabelaIO(int tabela[][3], int linhas) {
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d\t", tabela[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+FILA* filaAltaPrioridade;
+FILA* filaBaixaPrioridade;
+FILA* filaIO;
+
 
 int main(){
 	srand(time(NULL));
 	qtdeProcessos = QTDE_PROC;
-	processosTerminados = 0;
+	int processosTerminados = 0;
 	
-	int tabela_IO[QTDE_PROC][3];
-	int tabela_processos [QTDE_PROC][2];
-
+	int tabela_IO[QTDE_PROC][3] = {{0}};
+	int tabela_processos[QTDE_PROC][2];
 	
-
-    
     // Algoritmo RR com Feedback
     printf("### SIMULADOR ###\n");
   	printf("Condicao de corrida:\n");
   	
-  	// CondiÁ„o de corrida e IO's
+  	// Condicao de corrida e IO's
+  	
+  	// Mostrar os processos, seus tempos de serviÔøΩo e seus IO
   	printf("-------------------------------------------\n");
   	FILA* listaProcessos = gera_processos();
   	PCB* processo = listaProcessos->fim;
     for(int i = 0; i < qtdeProcessos; i++){
     	tabela_processos[i][0] = processo->pid;
     	tabela_processos[i][1] = processo->criacao;
-    	printf("Processo %i com tempo de CPU = %i u.t.", processo->pid, processo->tempo_serv);
+    	printf("P%i com tempo de servico = %i u.t.", processo->pid, processo->tempo_serv);
     	if(processo->io->tipo){
     		if(processo->io->tipo == 'D')
-    			printf(", IO = Disco e inicio em %i u.t.\n", processo->io->inicio);
+    			printf(", IO de Disco em %i u.t.\n", processo->io->inicio);
     		else if(processo->io->tipo == 'F')
-    			printf(", IO = Fita e inicio em %i u.t.\n", processo->io->inicio);
+    			printf(", IO de Fita em %i u.t.\n", processo->io->inicio);
     		else if(processo->io->tipo == 'I')
-    			printf(", IO = Impressora e inicio em %i u.t.\n", processo->io->inicio);
+    			printf(", IO de Impressora em %i u.t.\n", processo->io->inicio);
     	}
     	else printf("\n");
     	processo = processo->prox;
     }
     printf("-------------------------------------------\n");
+    
+    // Mostra tabela dos instantes de entrada dos processos
+    printf("|PROCESSO|INSTANTE DE ENTRADA|\n");
     imprimirTabela(tabela_processos, QTDE_PROC);
     
+    // Inicio simulacao
+    int instanteAtual = 0;
+    int temProcessoExecutar; // Var booleana para verificar se h· processo pra executar
+    int tempoDecorrido = 0;
+    processo = listaProcessos->fim;
+
+	filaAltaPrioridade = aloca_fila();
+	filaBaixaPrioridade = aloca_fila();
+	filaIO = aloca_fila();
+
+    while(processosTerminados < QTDE_PROC){
+    	instanteAtual += tempoDecorrido;
+    	
+    	// Verifica se chegou algum processo novo e se sim o coloca no fim da fila de alta prioridade
+    	if(processo->criacao <= instanteAtual){
+    		PCB* novoProcesso = (PCB *) malloc(sizeof(PCB));
+    		
+    		novoProcesso->ant = NULL;
+    		novoProcesso->prox = NULL;
+    		novoProcesso->criacao = processo->criacao;
+    		novoProcesso->tempo_serv = processo->tempo_serv;
+    		novoProcesso->io = processo->io;
+    		novoProcesso->pid = processo->pid;
+    		
+    		
+    		
+			adiciona_processo_fila(filaAltaPrioridade, novoProcesso);
+			printf("Novo processo P%d criado no instante %d\n\n", novoProcesso->pid, novoProcesso->criacao);
+			processo = processo->prox;
+		}
+		
+		// Verificar se algum IO terminou e colocar na fila adequada
+		
+		
+		// Executa processo
+		temProcessoExecutar = (filaAltaPrioridade->qtde) + (filaBaixaPrioridade->qtde);
+		
+		
+		if(!temProcessoExecutar){
+			tempoDecorrido = 1;
+		}else{
+			// verificar se tem IO
+			if(filaAltaPrioridade->qtde > 0){
+				PCB* processoExecutar = retira_processo_fila(filaAltaPrioridade);
+				
+				printf("---Infos processo---\n");
+				printf("Criacao = %d\n", processoExecutar->criacao);
+				printf("Tempo_serv = %d\n", processoExecutar->tempo_serv);
+				printf("PID = %d\n", processoExecutar->pid);
+				printf("--------------------------\n\n");
+				
+				
+				
+				
+				printf("Processo P%d comecou a executar em %d\n", processoExecutar->pid, instanteAtual);
+				int inicioIO = processoExecutar->io->inicio;
+				if(inicioIO != -1){
+					if(inicioIO - QUANTUM <= 0){
+						tempoDecorrido = inicioIO;
+						processoExecutar->tempo_serv -= tempoDecorrido;
+						processoExecutar->io->inicio = -1;
+						printf("Processo executou por %d\n", tempoDecorrido);
+						
+						adiciona_processo_fila(filaIO, processoExecutar);
+						tabela_IO[processoExecutar->pid - 1][0] = processoExecutar->pid;
+						tabela_IO[processoExecutar->pid - 1][1] = instanteAtual + tempoDecorrido;
+						tabela_IO[processoExecutar->pid - 1][2] = (instanteAtual + tempoDecorrido) + processoExecutar->io->duracao;
+						
+						printf("Processo pediu solicitacao de IO do tipo %c no instante %d\n\n", processoExecutar->io->tipo, (instanteAtual + tempoDecorrido));
+					}
+					else{
+						processoExecutar->io->inicio -= QUANTUM; // adptar inicio
+						processoExecutar->tempo_serv -= QUANTUM; // quanto ainda falta de temp_serv
+						tempoDecorrido = QUANTUM;
+						printf("Processo executou por %d\n", tempoDecorrido);
+						
+						adiciona_processo_fila(filaBaixaPrioridade, processoExecutar);
+						printf("Processo %d sofreu preempcao no instante %d\n\n", processoExecutar->pid, (instanteAtual + tempoDecorrido));
+					}
+				}else{
+					if(processoExecutar->tempo_serv - QUANTUM <= 0){
+						tempoDecorrido = processoExecutar->tempo_serv;
+						processoExecutar->tempo_serv = 0;
+						printf("Processo executou por %d\n", tempoDecorrido);
+					
+						printf("Processo %d terminou a execucao no instante %d\n\n", processoExecutar->pid, (instanteAtual + tempoDecorrido));
+					}
+					else{
+						processoExecutar->tempo_serv -= QUANTUM;
+						tempoDecorrido = QUANTUM;
+						printf("Processo executou por %d\n", tempoDecorrido);
+					
+						adiciona_processo_fila(filaBaixaPrioridade, processoExecutar);
+						printf("Processo %d sofreu preempcao no instante %d\n\n", processoExecutar->pid, (instanteAtual + tempoDecorrido));
+					}
+				}
+			}else{
+				tempoDecorrido = 1;
+				printf("ELSE\n\n");
+			}
+		}
+    	
+		processosTerminados++;
+	} // Fim while
+    
+    imprimirFila(filaAltaPrioridade);
+    printf("%d", filaAltaPrioridade->qtde);
+    printf("\n\nTABELA IO\n");
+    
+    imprimirTabelaIO(tabela_IO, QTDE_PROC);
     
     /*
-    int tempoPassado = 0;
-   	int tempoTotal = 0;
-    while(qtdeProcessos > 0){
-    	tempoTotal += tempoPassado;
-    	printf("\n\n0: Instante %i u.t.:\n", tempoTotal);
-    	imprime_status();
-    	if(filaAltaPrioridade->numProcessos > 0){ // Procura processos para executar na fila de alta prioridade
-    		processo = filaAltaPrioridade->base;
-    		tempoPassado = executa_processo(processo);
-    		printf("6: Processo %i ficou %is executando\n", processo->pid, tempoPassado);
-    		passa_tempo_io(tempoPassado);
-    		if(processo->tempo == -1)
-    			termina_processo(processo, filaAltaPrioridade);
-    		else{
-    			if(processo->io->tipo == 'D' && processo->io->inicio == -1){
-    				printf("7: Processo %i solicitou IO (Disco) na fila de alta prioridade\n", processo->pid);
-    				processo->io->tipo = 0;
-    				troca_fila(processo, filaAltaPrioridade, filaIoDisco);
-    			}
-    			else if(processo->io->tipo == 'F' && processo->io->inicio == -1){
-    				printf("7: Processo %i solicitou IO (Fita) na fila de alta prioridade\n", processo->pid);
-    				processo->io->tipo = 0;
-    				troca_fila(processo, filaAltaPrioridade, filaIoFita);
-    			}
-    			else if(processo->io->tipo == 'I' && processo->io->inicio == -1){
-    				printf("7: Processo %i solicitou IO (Impressora) na fila de alta prioridade\n", processo->pid);
-    				processo->io->tipo = 0;
-    				troca_fila(processo, filaAltaPrioridade, filaIoImpressora);
-    			}
-    			else troca_fila(processo, filaAltaPrioridade, filaBaixaPrioridade);
-    			printf("8: Processo %i foi bloqueado com %is restantes\n", processo->pid, processo->tempo);
-    		}
-    	}
-    	else if(filaBaixaPrioridade->numProcessos > 0){ // Procura processos para executar na fila de baixa prioridade
-    		processo = filaBaixaPrioridade->base;
-    		tempoPassado = executa_processo(processo);
-    		printf("6: Processo %i ficou %is executando\n", processo->pid, tempoPassado);
-    		passa_tempo_io(tempoPassado);
-    		if(processo->tempo == -1)
-    			termina_processo(processo, filaBaixaPrioridade);
-    		else{
-    			if(processo->io->tipo == 'D' && processo->io->inicio == -1){
-    				printf("7: Processo %i solicitou IO (Disco) na fila de baixa prioridade\n", processo->pid);
-    				processo->io->tipo = 0;
-    				troca_fila(processo, filaBaixaPrioridade, filaIoDisco);
-    			}
-    			else if(processo->io->tipo == 'F' && processo->io->inicio == -1){
-    				printf("7: Processo %i solicitou IO (Fita) na fila de baixa prioridade\n", processo->pid);
-    				processo->io->tipo = 0;
-    				troca_fila(processo, filaBaixaPrioridade, filaIoFita);
-    			}
-    			else if(processo->io->tipo == 'I' && processo->io->inicio == -1){
-    				printf("7: Processo %i solicitou IO (Impressora) na fila de baixa prioridade\n", processo->pid);
-    				processo->io->tipo = 0;
-    					troca_fila(processo, filaBaixaPrioridade, filaIoImpressora);
-    			}
-    			else if(processo->prox != NULL){
-    				PCB* base = processo->prox;
-					base->ant = NULL;
-					processo->prox = NULL;
-					filaBaixaPrioridade->topo->prox = processo;
-						filaBaixaPrioridade->topo = processo;
-					filaBaixaPrioridade->base = base;
-    			}
-    			printf("8: Processo %i foi bloqueado com %is restantes\n", processo->pid, processo->tempo);
-    		}
-    	}
-    	else{ // Todos os processos est√£o em IO
-    		tempoPassado = passa_tempo_io(-1); // Passa tempo at√© um processo estar pronto para executar
-    	}
-    }
-    printf("\nTempo total do simulador: %is\n", tempoTotal+tempoPassado);
     free(filaAltaPrioridade);
-    free(filaBaixaPrioridade);
-    free(filaIoDisco);
-    free(filaIoFita);
-    free(filaIoImpressora);
-    */
+	free(filaBaixaPrioridade;
+	free(filaIO);
+	*/
 	
 	return 0;
 }
